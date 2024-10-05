9df3a16a301c46377a1dae9797e14564531e14ac
seeck3 <marco.dseo@gmail.com>
2024-10-05 10:29:10 -0400

    prompt installed

---
+++ index.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+console.log("Hello let's print your name")
+
+const name = prompt("what is your name? ")
+
+const age = prompt("how old are you? ")
+
+const job = prompt("what do you do for living? ")
+
+console.log("my name is " + name + " i am " + age + "years old" + "my job is " + job)
